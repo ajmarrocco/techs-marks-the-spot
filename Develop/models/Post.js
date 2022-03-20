1ffa76a87ff8d0efeefc770f64ec3b40db000756
ajmarrocco <ajmarrocco@gmail.com>
2022-03-19 23:53:23 -0400

    post model created

---
+++ Develop/models/Post.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    {
+        // define an id column
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        // define a title column
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define a post_url column
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // if allowNull is set to false, we can run our data through validators before creating the table data
+            validate: {
+                isURL: true
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
